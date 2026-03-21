68ba5508d9088dc531df66e80856398a5b4420e9
Lucas Christian <lucaschgf7@gmail.com>
2026-03-21 15:40:46 -0300

    chore: configura verificador estático de código

---
+++ tools/quality/stylelint.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('stylelint').Config} */
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
+  reportDescriptionlessDisables: true,
+  reportInvalidScopeDisables: true,
+  reportNeedlessDisables: true,
+  reportUnscopedDisables: true,
+  rules: {
+    'no-empty-source': null,
+    'selector-class-pattern': null,
+  },
+};
