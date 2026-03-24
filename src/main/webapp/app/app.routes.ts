331f3ef5c0cf16f06932e3a9ca86f203ff449e72
vitorez <joaopsn06@gmail.com>
2026-03-23 23:17:49 -0300

    feat: add registration component with routing and form functionality

--- src/main/webapp/app/app.routes.ts
+++ src/main/webapp/app/app.routes.ts
@@ -6,6 +6,11 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent),
   },
   {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then((m) => m.RegisterComponent),
+  },
+  {
     path: '',
     loadComponent: () =>
       import('./layouts/public-layout/public-layout.component').then(
