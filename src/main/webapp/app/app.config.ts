f4b29decb9714e8907663eaf0758c66cfebad3e2
Lucas Christian <lucaschgf7@gmail.com>
2026-03-24 21:35:27 -0300

    feat: configura configuraçao global

--- src/main/webapp/app/app.config.ts
+++ src/main/webapp/app/app.config.ts
@@ -1,9 +1,42 @@
 import type { ApplicationConfig } from '@angular/core';
 import { provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 
+const MedflowPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{violet.50}',
+      100: '{violet.100}',
+      200: '{violet.200}',
+      300: '{violet.300}',
+      400: '{violet.400}',
+      500: '{violet.500}',
+      600: '{violet.600}',
+      700: '{violet.700}',
+      800: '{violet.800}',
+      900: '{violet.900}',
+      950: '{violet.950}',
+    },
+  },
+});
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: MedflowPreset,
+        options: {
+          darkModeSelector: 'app-dark',
+        },
+      },
+    }),
+    provideRouter(routes),
+  ],
 };
